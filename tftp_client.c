d57079d0ab70ec0115f4deb7764fc550830bebf1
Hahahahx <1219654535@qq.com>
2021-07-12 16:52:48 +0800

    fix:err

--- tftp_client.c
+++ tftp_client.c
@@ -110,7 +110,7 @@ int do_download(int sockfd, struct sockaddr_in serveraddr, char *filename)
     // 构建读写请求，给服务器发送的tftp指令并发送给服务器,例如：01test.txt0octet0
     text_len = sprintf(text, "%c%c%s%c%s%c", 0, 1, filename, 0, "octet", 0);
 
-    printf("send msg ");
+     printf("send msg ");
     puts(text);
     if (sendto(sockfd, text, text_len, 0, (struct sockaddr *)&serveraddr, addrlen) < 0)
     {
